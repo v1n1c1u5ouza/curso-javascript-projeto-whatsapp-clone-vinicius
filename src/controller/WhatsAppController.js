09ada7a192b3fc3702037182b59bd4312d27d45a
v1n1c1u5ouza <viniciusdesouzamarinho@gmail.com>
2022-10-06 10:20:29 -0300

    Add: Method elementsPrototype

--- src/controller/WhatsAppController.js
+++ src/controller/WhatsAppController.js
@@ -21,5 +21,56 @@ class WhatsAppController {
 
     }
 
+    elementsPrototype(){
+
+        Element.prototype.hide = function(){
+            this.style.display = 'none';
+            return this;
+        }
+
+        Element.prototype.show = function(){
+            this.style.display = 'block';
+            return this;
+        }
+
+        Element.prototype.toggle = function(){
+            this.style.display = (this.style.display === 'none') ? 'block' : 'none';
+            return this;
+        }
+
+        Element.prototype.on = function(events, fn) {
+            events.split(' ').forEach(event =>{
+                this.addEventListener(event, fn);
+            });
+            return this;
+        }
+
+        Element.prototype.css = function(style,) {
+            for (let name in style) {
+                this.style[name] = style[name];
+            }     
+            return this;
+        }
+
+        Element.prototype.addClass = function(name){
+            this.classList.add(name);
+            return this;
+        }
+
+        Element.prototype.removeClass = function(name){
+            this.classList.remove(name);
+            return this;
+        }
+
+        Element.prototype.toggleClass = function(name){
+            this.classList.toggle(name);
+            return this;
+        }
+
+        Element.prototype.hasClass = function(name){
+            return this.classList.contains(name);
+        } 
+    }
+
 
 }
