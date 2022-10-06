3c4434da77f2c35934e04568465f43637784910a
v1n1c1u5ouza <viniciusdesouzamarinho@gmail.com>
2022-10-06 10:19:04 -0300

    Add: Method to transform Ids do camelCase

--- src/controller/WhatsAppController.js
+++ src/controller/WhatsAppController.js
@@ -4,6 +4,22 @@ class WhatsAppController {
 
         console.log('WhatsAppController OK');
 
+        this.elementsPrototype(); 
+        this.loadElements();
+
     }
+
+    loadElements(){
+
+        this.el = {};
+
+        document.querySelectorAll(['[id]']).forEach(element=>{
+
+            this.el[Format.getCamelCase(element.id)] = element;
+
+        });
+
+    }
+
 
 }
