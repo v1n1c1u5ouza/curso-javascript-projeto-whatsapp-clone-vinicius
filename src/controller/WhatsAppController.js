1cb6bc9044471b990dccba6ce8b80bc099bab5e0
v1n1c1u5ouza <viniciusdesouzamarinho@gmail.com>
2022-10-06 11:20:11 -0300

    Add: Method to EditProfile and AddNewContact

--- src/controller/WhatsAppController.js
+++ src/controller/WhatsAppController.js
@@ -6,6 +6,7 @@ class WhatsAppController {
 
         this.elementsPrototype(); 
         this.loadElements();
+        this.initEvents();
 
     }
 
@@ -72,5 +73,50 @@ class WhatsAppController {
         } 
     }
 
+    initEvents(){
+
+        this.el.myPhoto.on('click', e=>{
+
+            this.closeAllLeftPanel();
+            this.el.panelEditProfile.show();
+            setTimeout(()=>{
+                this.el.panelEditProfile.addClass('open');
+            }, 300);
+            
+
+        });
+
+        this.el.btnNewContact.on('click', e=>{
+
+            this.closeAllLeftPanel();
+            this.el.panelAddContact.show();
+            setTimeout(()=>{
+                this.el.panelAddContact.addClass('open');
+            }, 300);
+            
+
+        });
+
+        this.el.btnClosePanelEditProfile.on('click', e=>{
+
+            this.el.panelEditProfile.removeClass('open');
+
+        })
+
+        this.el.btnClosePanelAddContact.on('click', e=>{
+
+            this.el.panelAddContact.removeClass('open');
+            
+        })
+
+    }
+
+    closeAllLeftPanel(){
+
+        this.el.panelAddContact.hide();
+        this.el.panelEditProfile.hide();
+
+
+    }
 
 }
