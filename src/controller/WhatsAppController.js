7ed408551c6ee5b8d48f1121adc573511ed411cf
v1n1c1u5ouza <viniciusdesouzamarinho@gmail.com>
2022-10-07 11:39:26 -0300

    Add: Obtaining Left panel data if formData

--- src/controller/WhatsAppController.js
+++ src/controller/WhatsAppController.js
@@ -71,6 +71,28 @@ class WhatsAppController {
         Element.prototype.hasClass = function(name){
             return this.classList.contains(name);
         } 
+
+        HTMLFormElement.prototype.getForm = function () {
+
+            return new FormData(this);
+
+        }
+
+
+        HTMLFormElement.prototype.toJSON = function () {
+
+            let json = {};
+
+            this.getForm().forEach((value, key)=>{
+
+                json[key] = value; 
+
+            });
+
+            return json;
+
+        }
+
     }
 
     initEvents(){
@@ -108,6 +130,39 @@ class WhatsAppController {
             this.el.panelAddContact.removeClass('open');
             
         })
+
+        this.el.photoContainerEditProfile.on('click', e=>{
+
+            this.el.inputProfilePhoto.click();  
+
+        });
+
+        this.el.inputNamePanelEditProfile.on('keypress', e=>{
+
+            if (e.key === 'Enter') {
+
+                e.preventDefault();
+                this.el.btnSavePanelEditProfile.click();
+
+            }
+
+        });
+
+        this.el.btnSavePanelEditProfile.on('click', e=>{
+
+            console.log(this.el.inputNamePanelEditProfile.innerHTML);
+
+        });
+
+        this.el.formPanelAddContact.on('submit', e=>{
+
+            e.preventDefault();
+            
+            let formData = new formData(this.el.formPanelAddContact);
+
+
+
+        });
 
     }
 
