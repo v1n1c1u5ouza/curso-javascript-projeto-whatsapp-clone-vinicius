d509772df8fd45e8aa912a83d8410cdebdc76043
v1n1c1u5ouza <viniciusdesouzamarinho@gmail.com>
2022-10-10 12:11:34 -0300

    Add: Open camera method

---
+++ src/controller/CameraController.js
@@ -0,0 +1,15 @@
+class CameraController {
+
+    constructor(videoEl) {
+        this._videoEl = videoEl
+ 
+        navigator.mediaDevices.getUserMedia({video:true}).then(screenStream=>{
+            this._screenStream = screenStream     
+            this._videoEl.srcObject = screenStream;
+            this._videoEl.play()
+        }).catch(err => {
+            console.error(err)
+        })
+    }
+
+}
