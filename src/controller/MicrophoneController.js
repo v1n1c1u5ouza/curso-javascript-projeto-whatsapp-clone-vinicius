d53e91cf3d24d21a612475130793ca6dcf6fac1a
v1n1c1u5ouza <viniciusdesouzamarinho@gmail.com>
2022-10-18 10:51:06 -0300

    Create: Class to capture microphne audio

---
+++ src/controller/MicrophoneController.js
@@ -0,0 +1,27 @@
+export class MicrophoneController {
+
+    constructor(audioEl) {
+        this._audioEl = audioEl;
+        navigator.mediaDevices.getUserMedia({
+            audio: true
+        }).then(stream => {
+            this._stream = stream;
+ 
+            let audio = new Audio();
+            audio.srcObject = stream;
+            audio.play();
+            
+        }).catch(err => {
+            console.error(err);
+        });
+    }
+
+    stop(){
+
+        this._stream.getTracks().forEach(track=>{
+            track.stop();
+        });
+
+    }
+
+}
